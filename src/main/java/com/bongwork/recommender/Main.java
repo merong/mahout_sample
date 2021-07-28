1b1e25b8fa3a0b19d806435c27f0fc3635f7f08a
tbkim <tbkim@leadersipiens.com>
2021-07-28 14:14:02 +0900

    initial commit

---
+++ src/main/java/com/bongwork/recommender/Main.java
@@ -0,0 +1,36 @@
+package com.bongwork.recommender;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException, TasteException {
+        DataModel model = new FileDataModel(new File("intro.csv"));
+        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+        UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1,
+                similarity, model);
+        UserBasedRecommender recommender = new GenericUserBasedRecommender(
+                model, neighborhood, similarity);
+
+        List<RecommendedItem> recommendations = recommender.recommend(3, 3);
+        for (RecommendedItem recommendation : recommendations) {
+            System.out.println(recommendation);
+        }
+    }
+
+
+}
---
+++ src/main/java/com/bongwork/recommender/RecommenderEvaluator.java
@@ -0,0 +1,6 @@
+package com.bongwork.recommender;
+
+public class RecommenderEvaluator {
+
+
+}
