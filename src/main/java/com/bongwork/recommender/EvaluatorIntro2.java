1528ca6062815f46aa6f4b0946e18629b039a25a
tbkim <k9400275@gmail.com>
2021-07-29 23:30:16 +0900

    ua.base add

--- src/main/java/com/bongwork/recommender/EvaluatorIntro2.java
+++ src/main/java/com/bongwork/recommender/EvaluatorIntro2.java
@@ -7,6 +7,7 @@ import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommender
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.slopeone.SlopeOneRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
@@ -22,9 +23,9 @@ public class EvaluatorIntro2 {
         File modelFile = null;
         if (args.length > 0)
             modelFile = new File(args[0]);
-        if(modelFile == null || !modelFile.exists())
+        if (modelFile == null || !modelFile.exists())
             modelFile = new File("intro.csv");
-        if(!modelFile.exists()) {
+        if (!modelFile.exists()) {
             System.err.println("Please, specify name of file, or put file 'input.csv' into current directory!");
             System.exit(1);
         }
@@ -41,6 +42,15 @@ public class EvaluatorIntro2 {
                 return new GenericUserBasedRecommender(model, neighborhood, similarity);
             }
         };
+//
+//        RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+//
+//        RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
+//            @Override
+//            public Recommender buildRecommender(DataModel dataModel) throws TasteException {
+//                return new SlopeOneRecommender(dataModel);
+//            }
+//        };
         // Use 70% of the data to train; test using the other 30%.
         double score = evaluator.evaluate(recommenderBuilder, null, model, 0.7, 1.0);
         System.out.println(score);
